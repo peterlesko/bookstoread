452174d969f57b69b20b9d34516ffedb81698137
peterlesko <peterlesko@yahoo.com>
2020-03-11 11:42:01 +0000

    Update BookShelfSpec.java

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,4 +1,30 @@
 package bookstoread;
 
-public class BookShelfSpec {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("BookShelf")
+class BookShelfSpec {
+
+//    private BookShelfSpec(TestInfo testInfo) {
+//        System.out.println("Working on test " + testInfo.getDisplayName());
+//    }
+
+    @Test
+    @DisplayName("is empty when no book is added to it")
+    void shelfEmptyWhenNoBookAdded() throws Exception {
+                                                                // Arrange
+        BookShelf shelf = new BookShelf();
+                                                                //Actj
+        List<String> books = shelf.books();
+                                                                //Assert
+
+        assertTrue(books.isEmpty(), () -> "Bookshelf should be empty");
+
+    }
+
 }
